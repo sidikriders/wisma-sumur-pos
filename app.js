213eda37529d73ca643672bc6cf6e2682441e314
sidikriders <sidik@xwork.id>
2018-11-19 04:49:58 +0700

    add morgan add sequelize logging option add function delete expired session in starting app

--- app.js
+++ app.js
@@ -3,29 +3,32 @@ var bodyParser = require('body-parser')
 var Sequelize = require('sequelize')
 var cookieParser = require('cookie-parser')
 var session = require('express-session')
+var morgan = require('morgan')
 
 var appConfig = require('./app-config.js')
 var dbConfig = require('./config/config.json')[process.env.NODE_ENV || 'development']
 var apiController = require('./controller/api-controller')
 
 var SequelizeStore = require('connect-session-sequelize')(session.Store)
+var sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  dialect: dbConfig.dialect,
+  logging: () => process.env.NODE_ENV === 'development'
+})
+
+deleteExpiredSessions(sequelize)
+
 var app = express()
 
 app.set('view engine', 'pug')
-app.use(cookieParser())
+app.use(morgan('default'))
 app.use(session({
   secret: appConfig.sessionConfig.secret,
+  resave: false,
+  saveUninitialized: false,
   store: new SequelizeStore({
-    db: new Sequelize(
-      dbConfig.database,
-      dbConfig.username,
-      dbConfig.password, {
-        dialect: dbConfig.dialect
-      }
-    )
+    db: sequelize
   })
 }))
-
 app.use(bodyParser.json())
 app.use(express.static('assets'))
 
@@ -42,4 +45,18 @@ app.get((err, req, res, next) => {
   })
 })
 
-app.listen(3000, () => { console.log('Listening to port 300') })
+app.listen(3000, () => { console.log('Listening to port 3000') })
+
+function deleteExpiredSessions(sequelize) {
+  var model = sequelize['import']('./models/session.js')
+  sequelize.models[model.name] = model
+  sequelize.models.Session.destroy({
+    where: {
+      expires: {
+        [Sequelize.Op.lte]: new Date()
+      }
+    }
+  }).then(() => {
+    console.log(`Executing (default): DELETE FROM \`Sessions\` WHERE \`expires\` <= '${new Date().toISOString()}'`)
+  })
+}
