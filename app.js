99922a438b4361c78659bb7528075751dea4eb6c
sidikriders <sidik@xwork.id>
2018-11-18 23:18:18 +0700

    update app to use different controller for API use express statice use body parser

--- app.js
+++ app.js
@@ -1,64 +1,24 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
+
+var apiController = require('./controller/api-controller')
 
 app.set('view engine', 'pug')
-
+app.use(bodyParser.json())
 app.use(express.static('assets'))
 
-app.get('/', (req, res, next) => {
-  res.locals.roomList = [
-    {
-      tenant: 'A',
-      room: {
-        id: 1,
-        name: 'C01'
-      }
-    }, {
-      tenant: 'B',
-      room: {
-        id: 2,
-        name: 'C02'
-      }
-    }, {
-      tenant: 'C',
-      room: {
-        id: 3,
-        name: 'C03'
-      }
-    }, {
-      tenant: 'D',
-      room: {
-        id: 4,
-        name: 'C04'
-      }
-    }, {
-      tenant: 'E',
-      room: {
-        id: 5,
-        name: 'C05'
-      }
-    }
-  ]
-  res.locals.thisYear = new Date().getFullYear()
-  res.locals.monthList = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'Mei',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Okt',
-    'Nov',
-    'Des'
-  ]
-  res.render('home')
-})
+app.use('/api', apiController)
 
 app.get('*', (req, res, next) => {
   res.status(404).send('not found')
+})
+
+app.get((err, req, res, next) => {
+  res.status(502).send({
+    status: false,
+    data: err
+  })
 })
 
 app.listen(3000, () => { console.log('Listening to port 300') })
